cbcde76d95091d3a92322281675f061988ff9ad1
unknown <reganbarua2015@gmail.com>
2017-04-27 12:43:16 -0700

    added all file stucture

---
+++ live_Project/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liveProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the liveProjectApp
+ */
+angular.module('liveProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ live_Project/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liveProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the liveProjectApp
+ */
+angular.module('liveProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
